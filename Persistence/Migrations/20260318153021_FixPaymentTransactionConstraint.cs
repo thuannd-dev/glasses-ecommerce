a66cec48ed809bc5292ad2c5c98be7861a6a7602
thuannd-dev <nguyenduongthuan75321@gmail.com>
2026-03-18 22:32:24 +0700

    Back: Fixing DbContext allowing null transaction IDs for Pending payments

---
+++ Persistence/Migrations/20260318153021_FixPaymentTransactionConstraint.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPaymentTransactionConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Payment_Transaction_By_Method",
+                table: "Payments");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Payment_Transaction_By_Method",
+                table: "Payments",
+                sql: "\r\n                    (\r\n                        PaymentMethod IN (1, 4)\r\n                    )\r\n                    OR\r\n                    (\r\n                        PaymentMethod IN (2,3) AND (TransactionId IS NOT NULL OR PaymentStatus = 1)\r\n                    )\r\n                    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Payment_Transaction_By_Method",
+                table: "Payments");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Payment_Transaction_By_Method",
+                table: "Payments",
+                sql: "\r\n                    (\r\n                        PaymentMethod = 1\r\n                    )\r\n                    OR\r\n                    (\r\n                        PaymentMethod IN (2,3) AND TransactionId IS NOT NULL\r\n                    )\r\n                    OR\r\n                    (\r\n                        PaymentMethod = 4\r\n                    )\r\n                    ");
+        }
+    }
+}
