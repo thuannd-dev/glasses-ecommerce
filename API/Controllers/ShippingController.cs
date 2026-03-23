1e46d6e81324fa23691a7d0bb596f07c82ac962d
thuannd-dev <nguyenduongthuan75321@gmail.com>
2026-03-23 12:43:11 +0700

    Back: Fixing api pattern of shipping controller

--- API/Controllers/ShippingController.cs
+++ API/Controllers/ShippingController.cs
@@ -1,40 +1,26 @@
-using Application.Interfaces;
+using Application.Orders.Queries;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 [AllowAnonymous]
-[Route("api/shipping")]
 public class ShippingController : BaseApiController
 {
-    private readonly IGHNService _ghnService;
-
-    public ShippingController(IGHNService ghnService)
-    {
-        _ghnService = ghnService;
-    }
-
     [HttpGet("fee")]
     public async Task<IActionResult> GetShippingFee(
         [FromQuery] int districtId, 
         [FromQuery] string wardCode, 
         [FromQuery] int weight = 200, 
-        [FromQuery] decimal insuranceValue = 0)
+        [FromQuery] decimal insuranceValue = 0,
+        CancellationToken ct = default)
     {
-        if (districtId <= 0 || string.IsNullOrWhiteSpace(wardCode))
-        {
-            return BadRequest("districtId and wardCode are required.");
-        }
-
-        try
-        {
-            var fee = await _ghnService.CalculateShippingFeeAsync(districtId, wardCode, weight, insuranceValue);
-            return Ok(new { fee });
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        return HandleResult(await Mediator.Send(new CalculateShippingFee.Query 
+        { 
+            DistrictId = districtId,
+            WardCode = wardCode,
+            Weight = weight,
+            InsuranceValue = insuranceValue
+        }, ct));
     }
 }
